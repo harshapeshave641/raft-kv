0a94f4c6bc17b0d35dbc431b6379544842ed391e
Harsha Peshave <harshapeshave13@email.com>
2026-05-03 15:56:32 +0530

    test: update StateMachine methods to support namespaces and add isolation tests

--- internal/store/kv_test.go
+++ internal/store/kv_test.go
@@ -6,7 +6,7 @@ func TestStateMachine_ApplySetAndGet(t *testing.T) {
 	sm := NewStateMachine()
 
 	// Initial get should fail
-	_, ok := sm.Get("foo")
+	_, ok := sm.Get(DefaultNamespace, "foo")
 	if ok {
 		t.Errorf("Expected foo to not exist")
 	}
@@ -18,7 +18,7 @@ func TestStateMachine_ApplySetAndGet(t *testing.T) {
 	}
 
 	// Get should now return the value
-	val, ok := sm.Get("foo")
+	val, ok := sm.Get(DefaultNamespace, "foo")
 	if !ok || val != "bar" {
 		t.Errorf("Expected foo to be bar, got %v (ok=%v)", val, ok)
 	}
@@ -29,7 +29,7 @@ func TestStateMachine_ApplyDelete(t *testing.T) {
 	sm.Apply(Command{Type: CommandSet, Key: "foo", Value: "bar"})
 	sm.Apply(Command{Type: CommandDelete, Key: "foo"})
 
-	_, ok := sm.Get("foo")
+	_, ok := sm.Get(DefaultNamespace, "foo")
 	if ok {
 		t.Errorf("Expected foo to be deleted")
 	}
@@ -40,11 +40,11 @@ func TestStateMachine_KeysAndLen(t *testing.T) {
 	sm.Apply(Command{Type: CommandSet, Key: "k1", Value: "v1"})
 	sm.Apply(Command{Type: CommandSet, Key: "k2", Value: "v2"})
 
-	if sm.Len() != 2 {
-		t.Errorf("Expected length 2, got %d", sm.Len())
+	if sm.Len(DefaultNamespace) != 2 {
+		t.Errorf("Expected length 2, got %d", sm.Len(DefaultNamespace))
 	}
 
-	keys := sm.Keys()
+	keys := sm.Keys(DefaultNamespace)
 	if len(keys) != 2 {
 		t.Errorf("Expected 2 keys, got %d", len(keys))
 	}
@@ -61,5 +61,43 @@ func TestStateMachine_KeysAndLen(t *testing.T) {
 	
 	if !hasK1 || !hasK2 {
 		t.Errorf("Expected keys to contain k1 and k2, got %v", keys)
+	}
+}
+
+func TestStateMachine_NamespaceIsolation(t *testing.T) {
+	sm := NewStateMachine()
+
+	// Set same key in different namespaces
+	sm.Apply(Command{Type: CommandSet, Namespace: "ns1", Key: "foo", Value: "val1"})
+	sm.Apply(Command{Type: CommandSet, Namespace: "ns2", Key: "foo", Value: "val2"})
+
+	// Verify isolation
+	val1, _ := sm.Get("ns1", "foo")
+	val2, _ := sm.Get("ns2", "foo")
+
+	if val1 != "val1" {
+		t.Errorf("Expected ns1/foo to be val1, got %s", val1)
+	}
+	if val2 != "val2" {
+		t.Errorf("Expected ns2/foo to be val2, got %s", val2)
+	}
+
+	// Delete from one namespace
+	sm.Apply(Command{Type: CommandDelete, Namespace: "ns1", Key: "foo"})
+	
+	_, ok1 := sm.Get("ns1", "foo")
+	_, ok2 := sm.Get("ns2", "foo")
+
+	if ok1 {
+		t.Errorf("Expected ns1/foo to be deleted")
+	}
+	if !ok2 {
+		t.Errorf("Expected ns2/foo to still exist")
+	}
+
+	// Wipe whole namespace
+	sm.Apply(Command{Type: CommandDeleteNamespace, Namespace: "ns2"})
+	if sm.Len("ns2") != 0 {
+		t.Errorf("Expected ns2 to be empty after wipe")
 	}
 }
